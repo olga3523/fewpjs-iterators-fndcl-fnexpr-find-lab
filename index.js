2c987fc8b0264978e443123b2f285e059a90d116
OLGA <olichka23.85@gmail.com>
2021-09-23 10:51:28 -0400

    commit tree 33765e6f6b08c1877e31c0f489cf3c78ed2ccadf

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(data)
+{
+    let res = data.find((el) => {
+        return el.result === 'W';
+    })
+    if (res == undefined)
+        return res;
+    return res.year;
+}
